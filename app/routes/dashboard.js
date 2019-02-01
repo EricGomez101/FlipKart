b0ac53701bc9b10a23cd7a31be72ce9c2d48b31a
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-01 14:37:27 -0700

    added place holder model data to dashboard

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,4 +1,13 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  model() {
+    return {
+      items: [
+        { title: "hat", price: 11.99, quantity: 1 },
+        { title: "bat", price: 31.99, quantity: 1 }
+      ]
+    };
+  },
+  actions: {}
 });
