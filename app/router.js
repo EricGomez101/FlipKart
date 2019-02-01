007dcf4f4c9727bfdc24d5ae0eafb7eed6898c3a
Eric <eric_gomez_1@hotmail.com>
2019-02-01 12:30:37 -0800

    main set up is complete.

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('dashboard', { path: "/dashboard" });
   this.route('cart');
+  this.route('splash-page',  {path: '/'});
 });
 
 export default Router;
